e7ef7fe907b545ef3cc7ac6457c7911a06e5c009
Ajibola Salami <ayo.lolade@gmail.com>
2021-03-16 14:12:48 +0100

    add block of code to redirect to https on production server

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Inertia\Inertia;
 
@@ -25,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if (!app()->isLocal()){
+            URL::forceScheme('https');
+        }
         Schema::defaultStringLength(191);
 
         if (request()->is('backoffice/*')) {
