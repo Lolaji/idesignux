bd6d7c7f7ce722baa8c799227c061b8f89a41b21
Ajibola Salami <33169712+Lolaji@users.noreply.github.com>
2021-03-25 00:31:31 -0700

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'backoffice/*'],
+    'paths' => ['api/*', 'backoffice/*', 'login', 'register' 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
